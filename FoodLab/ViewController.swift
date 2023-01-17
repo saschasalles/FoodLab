8cca13d29a6be8fe0a20378fce91414c7ba1354c
Sascha Salles <sascha@luni.app>
2023-01-17 21:47:11 +0100

    Initial Commit

---
+++ FoodLab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodLab
+//
+//  Created by Sascha Sallès on 17/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
